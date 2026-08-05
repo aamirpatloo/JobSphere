e421bff7893b495299b24da0ba97f1d20c979671
Mohammad Aamir <aamir.tbs@gmail.com>
2026-08-05 21:59:13 +0530

    chore: configure tailwind css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Welcome to JobSphere
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
